7402a4d2b23804e8c5bec87bc7a078aee019423f
Harald Asmus <49565148+haraldar@users.noreply.github.com>
2022-05-08 20:44:35 +0200

    The HTML and JS file that the popup can navigate to.
    The opening of the fullreport files is triggered from the popup files upon pressing the "Get full report." button.
    This initial version just shows a simple overview over a GitHub profile.
    Work in progress.

---
+++ GitEmailExtension/fullreport.js
@@ -0,0 +1,60 @@
+import {GitEmail, Account, Repo} from './gitemail.js';
+
+const coherents_withcheck = {
+    "name" : "name",
+    "bio" : "bio",
+    "email" : "email",
+    "location" : "location",
+    "company" : "company",
+    "hireable" : "hireable",
+    "blog" : "blog",
+    "twitter" : "twitter_username",
+    "followers" : "followers",
+    "following" : "following",
+    "gists" : "public_gists",
+    "repos" : "public_repos"
+};
+
+const coherents_nocheck = {
+    "account" : "login",
+    "created" : "created_at",
+    "updated" : "updated_at"
+};
+
+window.onload = async function () {
+    await chrome.storage.local.get(["ghToReportOn"], async function (items) {
+        // Fill out the standard output fields with the passed json object.
+        const ghToReportOn = items["ghToReportOn"];
+        getElem("image").src = ghToReportOn["avatar_url"];
+        document.querySelector("#fullreport-title").textContent += (": " + ghToReportOn["login"]);
+
+        setMultipleText(coherents_nocheck, ghToReportOn, false);
+        setMultipleText(coherents_withcheck, ghToReportOn);
+
+        // Check if email has been correctly extracted, else obtain it from one of the commits.
+        const reportedUser = getElem("account").textContent;
+        const email = await new GitEmail(reportedUser).gitEmail();
+        if (email !== null && email !== true && email != false) {
+            const currentDisplayedEmail = getElem("email").textContent;
+            if (currentDisplayedEmail == "Not given.") getElem("email").textContent = email;
+        };
+        const repos = await new GitEmail(reportedUser).getUserRepos(Repo.OWNED, false);
+        console.log(repos);
+    });
+}
+
+const getElem = (selector) => document.querySelector(`#user-${selector}`);
+
+const notGivenOr = (elem) => elem ?? "Not given.";
+
+function setElemText (elem, key, data_dict, notGivenCheck = true){
+    getElem(elem).textContent = notGivenCheck ?
+        notGivenOr(data_dict[key]) :
+        data_dict[key];
+}
+
+function setMultipleText (elems_dict, data_dict, notGivenCheck = true) {
+    for (let elem in elems_dict) {
+        setElemText(elem, elems_dict[elem], data_dict, notGivenCheck);
+    }
+}
