0fb0c906e6a99e4ed07dede8fe10d4f50847534d
Harald Asmus <haraldar@protonmail.com>
2022-07-31 23:28:23 +0200

    refactor: Changed the code structure and added documentation.

--- GitEmailExtension/gitemail_elems.js
+++ GitEmailExtension/gitemail_elems.js
@@ -1,4 +1,5 @@
 let errored = false;
+let currentURL = null;
 
 
 const getUrlParts = function (delim = '/') {
@@ -6,35 +7,75 @@ const getUrlParts = function (delim = '/') {
 }
 
 
-const getUserFromUrl = function () {
-    const userPath = getUrlParts("github.com/")[1];
-    const user = (userPath.includes('?'))
-        ? userPath.split('?')[0]
-        : user;
-    return user;
+const getUrlPathParts = function (delim = '/') {
+    return window.location.pathname.split(delim);
 }
 
 
-const createTabButton = function (btnId, btnText, eventAction) {
+const getUserFromUrl = function () {
+    return getUrlPathParts()[1];
+}
+
+
+/**
+ * Creates a node element of a certain type and optionally sets attributes and child nodes.
+ * @param {string} type - The HTML element type.
+ * @param {object} attrs - The attributes to set for the element to be created.
+ * @param {list} children - The child nodes to append to the element.
+ * @param {EventListener} listener - The child nodes to append to the element.
+ * @returns {HTMLElement} The resulting node element.
+ */
+const createNodeElem = function (type, attrs = {}, children = [], listener = null) {
+
+    let elem = document.createElement(type);
+
+    Object.entries(attrs)
+        .forEach(attr => elem.setAttribute(...attr));
+
+    children.forEach(child => elem.appendChild(child));
+
+    if (listener !== null)
+        elem.addEventListener(
+            "click",
+            () => listener(),
+            false
+        );
+
+    return elem;
+
+}
+
+/**
+ * Adds a new tab to the GitHub UI.
+ * @param {string} btnId - The HTML id to use for the button in the website.
+ * @param {string} btnText - The text that is displayed on the button.
+ * @param {EventListener} listener - The EventListener for the tab.
+ */
+const addTabButton = function (btnId, btnText, listener) {
 
     const urlParts = getUrlParts();
     
-    if (document.getElementById(btnId) === null 
-        && urlParts.length < 5
-        && urlParts[urlParts.length - 1] !== "") {
+    if (
+        document.getElementById(btnId) !== null
+        || urlParts.length >= 5
+        || urlParts[urlParts.length - 1] === ""
+    ) return;
+            
+    const tabBtn = createNodeElem(
+        "button",
+        {
+            id: btnId,
+            class: "UnderlineNav-item js-responsive-underlinenav-item"
+        },
+        [
+            document.createTextNode(btnText)
+        ],
+        listener
+    );
 
-        const NAV = document.getElementsByClassName("UnderlineNav-body width-full p-responsive");
-        let tabBtn = document.createElement("button");
-        tabBtn.appendChild(document.createTextNode(btnText));
-        tabBtn.setAttribute("class", "UnderlineNav-item js-responsive-underlinenav-item");
-        tabBtn.setAttribute("id", btnId);
-        tabBtn.addEventListener(
-            "click",
-            () => eventAction(),
-            false);
-        if (NAV.length > 0) NAV[0].append(tabBtn);
+    const NAV = document.getElementsByClassName("UnderlineNav-body width-full p-responsive");
+    if (NAV.length > 0) NAV[0].append(tabBtn);
 
-    }
 }
 
 
@@ -49,30 +90,51 @@ const invitationsBtnAction = function () {
 }
 
 
+/**
+ * Create the GitEmail profile summary entry.
+ * @returns {boolean} true if element doesn't exist yet else false
+ */
 function createGitemailEntry () {
     
-    if (document.getElementById("gitemail-email-div") === null) {
+    if (document.getElementById("gitemail-email-div") === null)
+    {
 
         // Create the necessary elements.
         // img "gitemail-logo-svg" - Holding the svg logo.
-        let gitemail_icon = document.createElement("img");
-        gitemail_icon.setAttribute("id", "gitemail-logo-svg");
-        gitemail_icon.setAttribute("src", chrome.extension.getURL("gitemail_logo.svg"));
-        gitemail_icon.setAttribute("height", "16");
-        gitemail_icon.setAttribute("viewBox", "0 0 16 16");
+        const gitemail_icon = createNodeElem(
+            "img",
+            {
+                id: "gitemail-logo-svg",
+                src: chrome.extension.getURL("gitemail_logo.svg"),
+                height: "16",
+                viewBox: "0 0 16 16"
+            }
+        );
 
         // span "gitemail-user-email" - Holding the email info.
-        let gitemail_span = document.createElement("span");
-        gitemail_span.appendChild(document.createTextNode("Processing..."));
-        gitemail_span.setAttribute("id", "gitemail-user-email");
-        gitemail_span.setAttribute("style", "padding-left: 10px");
+        const gitemail_span = createNodeElem(
+            "span",
+            {
+                id: "gitemail-user-email",
+                style: "padding-left: 10px;"
+            },
+            [
+                document.createTextNode("Processing...")
+            ]
+        );
 
         // div "gitemail-email-div" - Containing the previous two.
-        let gitemail_entry = document.createElement("div");
-        gitemail_entry.setAttribute("id", "gitemail-email-div");
-        gitemail_entry.setAttribute("style", "padding-top: 4px");
-        gitemail_entry.appendChild(gitemail_icon);
-        gitemail_entry.appendChild(gitemail_span);
+        const gitemail_entry = createNodeElem(
+            "div",
+            {
+                id: "gitemail-email-div",
+                style: "padding-top: 4px;"
+            },
+            [
+                gitemail_icon,
+                gitemail_span
+            ]
+        );
 
         // Insert the elements into the required place in the dom.
         const NAV = document.getElementsByClassName("js-profile-editable-area d-flex flex-column d-md-block");
@@ -93,7 +155,7 @@ async function insertGitemailEmail () {
     if (user.includes('?')) user = user.split('?')[0];
 
     // Try get the user email.
-    try{
+    try {
         const gitEmail = await import(chrome.runtime.getURL("gitemail.js"));
         let userEmail = await (new gitEmail.GitEmail(user)).gitEmail();
         if (userEmail === true) userEmail = "No repos to scan.";
@@ -112,13 +174,26 @@ async function insertGitemailEmail () {
 
 
 function insertGitemailElements () {
-    if (errored === false) {
-        createTabButton("gitemail-gists-btn", "Gists", gistsBtnAction);
-        // createTabButton("gitemail-invitations-btn", "Invitations", invitationsBtnAction);
-        if (createGitemailEntry()) insertGitemailEmail();
+    if (currentURL === null) {
+        console.log("href: " + window.location.href);
+        console.log("hash: " + window.location.hash);
+        console.log("search: " + window.location.search);
+        console.log("pathname: " + window.location.pathname);
+
+        console.log("user: " + getUserFromUrl());
+    }
+    if (currentURL !== window.location.href) {
+        currentURL = window.location.href;
+        addTabButton("gitemail-gists-btn", "Gists", gistsBtnAction);
+        // addTabButton("gitemail-invitations-btn", "Invitations", invitationsBtnAction);
+        if (createGitemailEntry())
+            insertGitemailEmail();
     }
 }
 
 
 window.onload = () => insertGitemailElements();
-window.setInterval(() => insertGitemailElements(), 500);
+window.setInterval(
+    () => insertGitemailElements(),
+    500
+);
